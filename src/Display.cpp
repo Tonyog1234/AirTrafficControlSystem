8fe3a6bdbb4fae80e3e3f7da8e3d7f139bc5264d
Tony <tony0953112356@gmail.com>
2025-03-31 11:49:50 -0400

    update

--- src/Display.cpp
+++ src/Display.cpp
@@ -20,7 +20,7 @@ Display::~Display() {
 
 void Display::StartDisplayServer() {
     cout << "Display server start........" << endl;
-    attach = name_attach(NULL, "display_system", 0);
+    attach = name_attach(NULL, "DisplayInfo", 0);
     if (attach == NULL) {
         perror("[Display] name_attach failed");
         exit(EXIT_FAILURE);
@@ -39,37 +39,20 @@ void* Display::DisplayServerThread(void* arg) {
     Display* self = static_cast<Display*>(arg);
     while (true) {
         int rcvid;
-        msg_struct msg;
-        struct _msg_info info;
-        rcvid = MsgReceive(self->attach->chid, &msg, sizeof(msg), &info);
+        msg_struct msgFromComp;
+        //struct _msg_info info;
+        rcvid = MsgReceive(self->attach->chid, &msgFromComp, sizeof(msgFromComp), NULL);
         if (rcvid == -1) {
             perror("[Display] MsgReceive failed");
             continue;
         }
 
-        time_t now = time(NULL);
-        char time_str[26];
-        ctime_r(&now, time_str);
-        time_str[strlen(time_str) - 1] = '\0';  // Remove newline
-
-        // Log sender PID and message
-        cout << "[" << time_str << "] [Display] Received - Sender PID: " << info.pid
-             << ", ID: " << msg.id << ", Body: \"" << msg.body << "\"" << endl;
-
-        // Filter valid messages
-        if (strncmp(msg.body, "ID:", 3) == 0 || strncmp(msg.body, "Aircraft ID", 11) == 0) {
-            cout << "[" << time_str << "] [Display] Aircraft Info - " << msg.body << endl;
-        } else {
-            cout << "[" << time_str << "] [Display] Ignoring invalid message - Sender PID: " << info.pid
-                 << ", ID: " << msg.id << ", Body: \"" << msg.body << "\"" << endl;
-            MsgReply(rcvid, 0, NULL, 0);
-            continue;
-        }
-
-        msg_struct reply;
-        reply.id = msg.id;
-        strcpy(reply.body, "Displayed");
-        if (MsgReply(rcvid, 0, &reply, sizeof(reply)) == -1) {
+       cout<<"[Display] Receive message from computer "<<endl;
+       cout<<"[Display] Aircraft Info: "<<msgFromComp.body<<endl;
+        msg_struct replyToComp;
+        replyToComp.id = msgFromComp.id;
+        strcpy(replyToComp.body, "Displayed Successfully");
+        if (MsgReply(rcvid, 0, &replyToComp, sizeof(replyToComp)) == -1) {
             perror("[Display] MsgReply failed");
         }
     }
